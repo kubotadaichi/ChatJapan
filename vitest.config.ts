52755d1404f928517cd4fe65074b9cedf3533375
kubotadaichi <kubotadaichi202@gmail.com>
2026-02-22 09:17:16 +0900

    test: scope vitest discovery to src directory

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   test: {
+    dir: './src',
     environment: 'jsdom',
     setupFiles: ['./src/test/setup.ts'],
     globals: true,
